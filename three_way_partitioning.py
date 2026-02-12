e01d812792dbd80948f8333f12b10c3fe9cbdf26
KumarSwatantra <kumarswatantra943091@gmail.com>
2026-02-12 15:30:21 +0530

    Create three_way_partitioning.py

---
+++ three_way_partitioning.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeWayPartition(self, arr, a, b):
+        start = 0
+        end = len(arr) - 1
+        i = 0
+
+        while i <= end:
+            if arr[i] < a:
+                arr[i], arr[start] = arr[start], arr[i]
+                start += 1
+                i += 1
+
+            elif arr[i] > b:
+                arr[i], arr[end] = arr[end], arr[i]
+                end -= 1
+
+            else:
+                i += 1
+
+        return arr
