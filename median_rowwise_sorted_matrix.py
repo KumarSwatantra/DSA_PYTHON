cb6fd7ca44b1272bce1b0382ed5351e163d169cb
KumarSwatantra <kumarswatantra943091@gmail.com>
2026-02-16 23:51:48 +0530

    Create median_rowwise_sorted_matrix.py

---
+++ median_rowwise_sorted_matrix.py
@@ -0,0 +1,23 @@
+import bisect
+
+class Solution:
+    def median(self, matrix, r, c):
+        
+        low = min(row[0] for row in matrix)
+        high = max(row[-1] for row in matrix)
+        
+        desired = (r * c + 1) // 2
+        
+        while low < high:
+            mid = (low + high) // 2
+            
+            count = 0
+            for row in matrix:
+                count += bisect.bisect_right(row, mid)
+            
+            if count < desired:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
