8017f92d47e43981bbacb2241bee58ca33cf6148
KumarSwatantra <kumarswatantra943091@gmail.com>
2026-02-26 12:00:17 +0530

    Create reverse_array.py

---
+++ reverse_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseArray(self, arr, n):
+        left = 0
+        right = n - 1
+
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+
+        return arr
